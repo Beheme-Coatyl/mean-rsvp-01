7a9bce1a90401d366d851e22bc88cd662b4ad024
Beheme-Coatyl <32665542+Beheme-Coatyl@users.noreply.github.com>
2017-10-19 15:30:37 +0200

    Update auth.config.ts

--- src/app/auth/auth.config.ts
+++ src/app/auth/auth.config.ts
@@ -1,4 +1,5 @@
 // src/app/auth/auth.config.ts
+
 import { ENV } from './../core/env.config';
 
 interface AuthConfig {
@@ -8,13 +9,15 @@ interface AuthConfig {
   REDIRECT: string;
   SCOPE: string;
   NAMESPACE: string;
+  SILENT_REDIRECT: string;
 }
 
 export const AUTH_CONFIG: AuthConfig = {
   CLIENT_ID: '<AUTH0-client-id>',
   CLIENT_DOMAIN: '<AUTH0-client-domain>',
-  AUDIENCE: 'http://localhost:8083/api/', // likely http://localhost:8083/api/
+  AUDIENCE: '<API_SERVER>/api',
   REDIRECT: `${ENV.BASE_URI}/callback`,
   SCOPE: 'openid profile',
-  NAMESPACE: 'http://myapp.com/roles'
+  NAMESPACE: 'http://myapp.com/roles',
+  SILENT_REDIRECT: '<API_SERVER>/silent'
 };
