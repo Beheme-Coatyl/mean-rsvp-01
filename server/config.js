8ddbe86dda79d359c2b1dc4a8b18093b7421ed75
Beheme-Coatyl <32665542+Beheme-Coatyl@users.noreply.github.com>
2017-10-19 15:27:08 +0200

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 // server/config.js
 module.exports = {
-  AUTH0_DOMAIN: '<AUTH0-client-domain>', // e.g., kmaida.auth0.com
-  AUTH0_API_AUDIENCE: 'http://localhost:8083/api/', // e.g., 'http://localhost:8083/api/'
+  AUTH0_DOMAIN: '<AUTH0-client-domain>',
+  AUTH0_API_AUDIENCE: '<API_SERVER>/api',
   MONGO_URI: process.env.MONGO_URI || '<MONGODB-url>',
   NAMESPACE: 'http://myapp.com/roles'
 };
